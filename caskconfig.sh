2371b4b5e2ffc5c4ecea05cafd7d47ea179c04e8
m-parisotto <104174290+m-parisotto@users.noreply.github.com>
2022-04-22 14:27:18 +1000

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -1,18 +1,27 @@
+#Tells intepreter this is a script.
 #! /bin/sh
+
+#This installs Homebrew, which is used to install the programs.
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+#Delays for 10s to ensure background process completion.
 sleep 10
 
+#Adds Homebrew to PATH
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
 
+#Adds Homebrew to PATH
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+#This delay to ensure Homebrew is added to PATH - will throw an error if not.
 sleep 10
 
 brew tap homebrew/cask
 
+#Installs the cask module for Homebrew allowing the installation of Casks (Programs).
 brew install cask
 
+#Command for Homebrew installs of various programs.
 brew install google-chrome
 
 brew install slack
@@ -28,3 +37,8 @@ brew install microsoft-teams
 brew install zoom
 
 brew install google-drive-file-stream
+
+sleep 10
+
+#This rewrites the default screenshot location to a screenshot folder on the desktop.
+defaults write com.apple.screencapture location /Desktop/Screenshots 
